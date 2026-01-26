e54bbea7f3a451743f05158f510c347ce79e8021
Hendrik Reiter <hendrik.reiter@yahoo.com>
2026-01-26 07:18:55 +0100

    Implement a new trie data strucure which is more intuitive

--- algo/new_trie.py
+++ algo/new_trie.py
@@ -1,7 +1,8 @@
 from typing import List
 
+
 class NewTrie:
-    def __init__(self, label):
+    def __init__(self, label="#"):
         self.label = label
         self.children: List['NewTrie'] = []
 
@@ -11,18 +12,41 @@ class NewTrie:
     def has_child(self, label) -> bool:
         return label in [child.label for child in self.children]
 
+    def is_leaf(self):
+        return len(self.children) == 0
+
     def get_child(self, label) -> 'NewTrie':
         return [child for child in self.children if child.label == label][0]
+
+    def get_children(self):
+        return self.children
+
+    def has_child_with_label(self, label) -> bool:
+        return bool([child for child in self.children if child.label == label])
 
     def min(self):
         if not self.children:
             return 0
         return 1 + min([child.min() for child in self.children])
 
+    def traverse(self, label) -> 'NewTrie':
+        return [child for child in self.children if child.label == label][0]
+
     def mean(self):
         if not self.children:
             return 0
         return 1 + sum([child.mean() for child in self.children]) / len(self.children)
+
+    def __eq__(self, other):
+        if self.is_leaf() and other.is_leaf():
+            return self.label == other.label
+        if len(self.children) == len(other.children):
+            for child in self.children:
+                if not child in other.children:
+                    return False
+            return True
+        return False
+
 
 class TrieBuilder:
     def __init__(self, trie: NewTrie):
@@ -40,13 +64,24 @@ class TrieBuilder:
     def reset(self):
         self.active_trie = self.root_trie
 
+
 if __name__ == '__main__':
-        testee = TrieBuilder(NewTrie("#"))
-        testee.insert("A")
-        testee.insert("B")
-        testee.reset()
-        testee.insert("A")
-        testee.insert("D")
-        testee.insert("C")
-        testee.insert("E")
-        print(testee.root_trie.min())
+    testee = TrieBuilder(NewTrie("#"))
+    testee.insert("A")
+    testee.insert("B")
+    testee.insert("C")
+    testee.reset()
+    testee.insert("A")
+    testee.insert("B")
+    testee.insert("C")
+
+    testee2 = TrieBuilder(NewTrie("#"))
+    testee2.insert("A")
+    testee2.insert("B")
+    testee2.insert("D")
+    testee2.reset()
+    testee2.insert("A")
+    testee2.insert("B")
+    testee2.insert("C")
+    print(testee.root_trie == testee2.root_trie)
+    print(testee.root_trie.min())
